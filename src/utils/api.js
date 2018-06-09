d339d23f33ce16962a952065108626864ee01d0e
Noel Escobedo <noel@michelada.io>
2018-06-09 05:37:49 +0000

    Eliminamos codigo que no nos sirve para este ejemplo y agregamos el servidor de prueba

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,21 +1,13 @@
 class Api {
   static headers = async function(contentType) {
-    // const options = {
-    //   sharedPreferencesName: 'tallerRN',
-    //   keychainService: 'tallerRN'
-    // };
-    // const jwt = await SInfo.getItem('jwt', options);
-    // let auth_header = 'Bearer ' + jwt;
-
     return {
-      // Authorization: auth_header,
       'Content-Type': contentType || 'application/json'
     };
   };
 
   // Todas las llamadas pasan por aquí
   static xhr = async function(route, params, verb) {
-    const host = 'http://localhost:4000';
+    const host = 'https://notes-workshop.herokuapp.com/';
     const url = `${host}${route}`;
     const headers = await this.headers();
     const options = {
